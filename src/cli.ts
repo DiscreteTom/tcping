7f8afa85c97a4491ca9be15a236afdb2fdf7bd8f
DiscreteTom <discrete_tom@outlook.com>
2023-01-10 16:09:12 +0800

    feat: cli will handle ctrl-c and print stats

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,22 @@
 #!/usr/bin/env node
 
 import { run } from "./lib";
+import { statsToString } from "./stats";
 
-run();
+async function main() {
+  await Promise.any([
+    run(),
+    new Promise<void>((resolve, reject) => {
+      process.on("SIGINT", function () {
+        resolve();
+      });
+    }),
+  ]);
+
+  console.log(); // empty line
+  console.log(statsToString());
+
+  process.exit(0);
+}
+
+main();
